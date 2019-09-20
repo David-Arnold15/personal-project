c0718bd3af9ad72e5ad35b4c49d2a69c4b964e1b
David-Arnold15 <you@example.com>
2019-09-20 12:58:26 -0500

    new file:   imdbpage.txt 	modified:   web-Scraping-Functions.py

--- web-Scraping-Functions.py
+++ web-Scraping-Functions.py
@@ -1,9 +1,13 @@
 import requests as rq
 from bs4 import BeautifulSoup as bs
-def table_scrape(website):
-    r = rq.get(website, verify=False)
-    html_doc = ''
+def table_scrape(website, debug):
+    if debug == False:
+        r = rq.get(website, verify=False)
+        html_doc = r.content
+    else: 
+        f= open("imdbpage.txt","w+")
+        html_doc = f
     soup = bs(html_doc, 'html.parser')
-    print(r.text)
-    
-table_scrape("http://www.bing.com")
+    actor_table = soup.find_all('td', class_ = 'primary_photo' )
+    print(actor_table[0].findNextSibling('td'))
+table_scrape("https://www.imdb.com/title/tt4154756/?ref_=nv_sr_1?ref_=nv_sr_1", False)
