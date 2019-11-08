b5079c2f78df2c2bc019116236beff03a21c9781
David-Arnold15 <you@example.com>
2019-11-08 13:13:00 -0600

    modified:   Main.py 	modified:   face.jpg 	new file:   movies/Avengers_Infinity_War/Robert Downey JR.jpg 	modified:   webScrapingFunctions.py

--- Main.py
+++ Main.py
@@ -2,31 +2,38 @@ import pyautogui , os
 import face_recognition
 from pynput import keyboard
 #recognizing the faces themselves
+actor_success = ""
+movie  = "Avengers_Infinity_War"
 def recognize(movie):
-    movie  = "Avengers_Infinity_War"
+    actor_success= []
     files = os.listdir("movies/" + movie)
+    unknown_image = face_recognition.load_image_file("face.jpg")
+    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    reference = ""
+    reference_encoding = ""
+    results = ""
     for file in files:
         reference = face_recognition.load_image_file("movies/" + movie + "/" +file)
-        unknown_image = face_recognition.load_image_file("my_screenshot.jpg")
-        
-        
         reference_encoding = face_recognition.face_encodings(reference)[0]
-        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-    
+        
         results = face_recognition.compare_faces([reference_encoding], unknown_encoding)
-    print(results)
-
+        if results[0] == True:
+            actor_success.append(file[:-4])
+    
+    return actor_success
+    
 COMBINATIONS = [
     {keyboard.Key.shift, keyboard.KeyCode(char='a')},
     {keyboard.Key.shift, keyboard.KeyCode(char='A')}
     ]
 def screenshot_process():
-    im1 = pyautogui.screenshot('my_screenshot.jpg')
+    im1 = pyautogui.screenshot('face.jpg')
 screenshot_process()
 current = set()
 def execute():
     screenshot_process()
-    recognize("face.jpg")
+    recognized = recognize(movie)
+    print(recognized)
 def on_press(key):
     if any([key in COMBO for COMBO in COMBINATIONS]):
         current.add(key)
@@ -37,3 +44,5 @@ def on_release(key):
         current.remove(key)
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+recognized = recognize(movie)
+print(recognized)
--- webScrapingFunctions.py
+++ webScrapingFunctions.py
@@ -1,5 +1,6 @@
 import requests as rq
 from bs4 import BeautifulSoup as bs
+website = ""
 def table_scrape(website, debug):
     if debug == False:
         #makes an  html request to the given site
@@ -13,14 +14,20 @@ def table_scrape(website, debug):
     #print(bs.get_text(actor_table[0].findNextSibling('td')))
     #adds every actor to a list
     for i in range(len(actor_table)):
+        #follow a link off of the primary photo and get the images from it
+        link = actor_table[i].findParent('a')
+        print(link)
         actors.append(bs.get_text(actor_table[i].findNextSibling('td')))
     #removes whitespace characters
     for i in range(len(actors)):
         actors[i] = actors[i].strip()
     #print(actors)    
+    
+    #get the images of these actors
     return actors
                    
         
         
 if __name__ == "__main__":
-    print(table_scrape("https://www.imdb.com/title/tt2250912/?ref_=nv_sr_3?ref_=nv_sr_3", False))
+    website = input("please paste an imdb link for the movie you wish to watch: ")
+    print(table_scrape("https://www.imdb.com/title/tt4154756/?ref_=nv_sr_1?ref_=nv_sr_1" , False))
